48d6df4591b545499af0da50ff34c3196d01f662
ingyetejacob2002 <ingyetejacob2002@gmail.com>
2025-07-06 16:01:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def predict():
     return(render_template('index.html',placeholder=f'The predicted output is: {rounded}'))
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
