660749e9974f1235d955dc53b2417f5d88d75533
ingyetejacob2002 <ingyetejacob2002@gmail.com>
2025-07-06 14:33:46 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import joblib 
+model=joblib.load('C://Users/user/Desktop/prediction/model.joblib')
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    attendance = int(request.form['attendance'])
+    hours = int(request.form['hours'])
+    prediction=model.predict([[attendance,hours]])
+    rounded = round(prediction[0], 2)
+    return(render_template('index.html',placeholder=f'The predicted output is: {rounded}'))
+    
+if __name__ == '__main__':
+    app.run(debug=True)
